4f68294f0fa05dd505d0f5b6a3baa3186b942f24
Nattōsai Mitō <flow4re2c@gmail.com>
2024-08-17 10:13:10 +0900

    simplify 'examples/progress_spinner.py'

--- examples/progress_spinner.py
+++ examples/progress_spinner.py
@@ -1,46 +1,32 @@
+import itertools
+
 import tkinter as tk
 import asynctkinter as at
 
 
-async def run_progress_spinner(x1, y1, x2, y2, *, clock: at.Clock, draw_target: tk.Canvas, line_color='black', line_width=10):
-    start = 70
-    extent = 20
+async def run_progress_spinner(
+        x1, y1, x2, y2, *, clock: at.Clock, draw_target: tk.Canvas, line_color='black', line_width=10,
+        min_arc_angle=15, speed=1.0):
+    MA = min_arc_angle
+    BS = 20  # base speed
+    get_next_start = itertools.accumulate(itertools.cycle((BS, BS, BS + 360 - MA - MA,  BS, )), initial=90).__next__
+    get_next_extent = itertools.cycle((MA, 360 - MA, 360 - MA, MA, )).__next__
+    start = get_next_start()
+    extent = get_next_extent()
+
     arc = draw_target.create_arc(
         x1, y1, x2, y2,
         outline=line_color, width=line_width, start=start, extent=extent, style='arc',
     )
-
     try:
+        d = 0.5 / speed
         while True:
-            prev_v = 0
-            async for dt, __, p in clock.anim_with_dt_et_ratio(duration=0.7):
-                p = min(p, 1.0)
-                v = p * p * 320
-                diff = v - prev_v
-                extent += diff
-                start -= diff
-                start -= dt * 30
-                draw_target.itemconfig(arc, start=start, extent=extent)
-                prev_v = v
-            async for dt, et in clock.anim_with_dt_et():
-                start -= dt * 30
-                draw_target.itemconfig(arc, start=start)
-                if et >= 0.2:
-                    break
-            prev_v = 0
-            async for dt, __, p in clock.anim_with_dt_et_ratio(duration=0.7):
-                p = min(p, 1.0)
-                v = p * p * 320
-                diff = v - prev_v
-                extent -= diff
-                start -= dt * 30
-                draw_target.itemconfig(arc, start=start, extent=extent)
-                prev_v = v
-            async for dt, et in clock.anim_with_dt_et():
-                start -= dt * 30
-                draw_target.itemconfig(arc, start=start)
-                if et >= 0.2:
-                    break
+            next_start = get_next_start()
+            next_extent = get_next_extent()
+            async for s, e in clock.interpolate_sequence((start, extent), (next_start, next_extent), duration=d):
+                draw_target.itemconfig(arc, start=s, extent=e)
+            start = next_start
+            extent = next_extent
     finally:
         draw_target.delete(arc)
 
